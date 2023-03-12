5fc6d5a84c0ee255e8c49f08e41ee9e3a38b0ab4
Kaique Gama <kaiquesilva49@hotmail.com>
2023-03-12 16:25:08 -0300

    refactor

---
+++ src/main/java/br/alura/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package br.alura.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.alura.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
