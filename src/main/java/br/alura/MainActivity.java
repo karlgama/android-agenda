255234bcbb05b6010979bdde8d20492ef1f46cc1
Kaique Gama <kaiquesilva49@hotmail.com>
2023-03-12 10:57:59 -0300

    inser icon and appbar

--- src/main/java/br/alura/MainActivity.java
+++ src/main/java/br/alura/MainActivity.java
@@ -7,14 +7,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
         List<String> alunos = List.of("Alex", "Fran", "Jose");
         ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
         listaAlunos.setAdapter(
