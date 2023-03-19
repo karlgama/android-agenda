94669d07fefe21bf0f06d2d9a5f181626ca45807
Kaique Gama <kaiquesilva49@hotmail.com>
2023-03-19 13:03:38 -0300

    create aluno

---
+++ src/main/java/br/alura/model/Aluno.java
@@ -0,0 +1,25 @@
+package br.alura.model;
+
+public class Aluno {
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
--- src/main/java/br/alura/ui/activity/FormularioAlunoActivity.java
+++ src/main/java/br/alura/ui/activity/FormularioAlunoActivity.java
@@ -3,8 +3,13 @@ package br.alura.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.alura.R;
+import br.alura.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -12,5 +17,21 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome,telefone,email);
+                Toast.makeText(FormularioAlunoActivity.this, alunoCriado.getNome(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
