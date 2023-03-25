5bbcd02ba2cb6d5c131e681c376826210df2530e
Kaique Gama <kaiquesilva49@hotmail.com>
2023-03-25 16:50:37 -0300

    criando menu atraves de arquivo estático

--- src/main/java/br/alura/ui/activity/ListaAlunosActivity.java
+++ src/main/java/br/alura/ui/activity/ListaAlunosActivity.java
@@ -48,14 +48,18 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add("Remover");
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Aluno aluno = adapter.getItem(menuInfo.position);
-        remove(aluno);
+        int id = item.getItemId();
+        if(id == R.id.activity_lista_alunos_menu_remover){
+            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            Aluno aluno = adapter.getItem(menuInfo.position);
+            remove(aluno);
+        }
+
         return super.onContextItemSelected(item);
     }
 
